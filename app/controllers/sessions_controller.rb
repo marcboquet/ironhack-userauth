c6c1d606cf5e85e4531e571edee60a9ebf947fae
Marc Boquet <marc@apalan.cat>
2014-10-09 10:01:48 +0200

    Check for user password and display result
    It will render one of these messages:
    PASSWORD OK
    PASSWORD NOT OK
    USER NOT FOUND

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    # Check if user with email exists
+    user = User.find_by_email(params[:email])
+    if user
+      if user.authenticate(params[:password])
+        render text: "PASSWORD OK"
+      else
+        render text: "PASSWORD NOT OK"
+      end
+    else
+      render text: "USER NOT FOUND"
+    end
+  end
 end
