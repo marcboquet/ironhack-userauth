53ee3800d0e3443e27062ee99d4a534ef8a5a5ad
Marc Boquet <marc@apalan.cat>
2014-10-09 10:10:15 +0200

    Authenticate current user in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,15 @@ class SessionsController < ApplicationController
   def create
     # Check if user with email exists
     user = User.find_by_email(params[:email])
-    if user
-      if user.authenticate(params[:password])
-        render text: "PASSWORD OK"
-      else
-        render text: "PASSWORD NOT OK"
-      end
+    if user &&user.authenticate(params[:password])
+      # Password is correct
+      # Set the session to current user
+      # Will use cookie
+      session[:user_id] = user.id
+      redirect_to root_url
     else
-      render text: "USER NOT FOUND"
+      # There's no user or the password is incorrect
+      render :new
     end
   end
 end
