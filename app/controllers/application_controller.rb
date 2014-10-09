ac26dcda71751a60927b3f8e5313b33842d598ce
Marc Boquet <marc@apalan.cat>
2014-10-09 11:40:57 +0200

    Implement global current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  # Provide a global current_user() method to
+  # check if there's a user logged in from
+  # any controller or view
+  def current_user
+    if session[:user_id]
+      # If current user is set in the session
+      # return the current user
+      @current_user ||= User.find(session[:user_id])
+    end
+  end
+
+  # Indicate that the current_user() should be
+  # a helper (accessible from views)
+  helper_method :current_user
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    if session[:user_id]
-      # If current user is set in the session
-      # prepare a variable for the view
-      @current_user = User.find(session[:user_id])
-    end
   end
 end
