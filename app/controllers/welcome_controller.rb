19a2e87eacd72b67da30783e51459141cef078eb
Marc Boquet <marc@apalan.cat>
2014-10-09 10:59:15 +0200

    Display welcome message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    if session[:user_id]
+      # If current user is set in the session
+      # prepare a variable for the view
+      @current_user = User.find(session[:user_id])
+    end
   end
 end
